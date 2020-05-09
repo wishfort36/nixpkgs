3a4e7a390bd8c609f569360b8aa9b9e6c81628ec
Frederik Rietdijk <fridh@fridh.nl>
2020-05-09 11:34:00 +0200

    python3.pkgs.flit-core: init at 2.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2809,6 +2809,8 @@ in {
 
   flit = callPackage ../development/python-modules/flit { };
 
+  flit-core = callPackage ../development/python-modules/flit-core { };
+
   flowlogs_reader = callPackage ../development/python-modules/flowlogs_reader { };
 
   fluent-logger = callPackage ../development/python-modules/fluent-logger {};
